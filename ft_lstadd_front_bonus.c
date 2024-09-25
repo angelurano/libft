e748baae22e4094c7543c9f607f0376ddd4120c6
Miguel Angel Garcia Beltran <migugar2@c2r7s6.42madrid.com>
2024-09-25 18:09:32 +0200

    Fix norminette errors

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -1,20 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: migugar2 <migugar2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 19:09:52 by migugar2          #+#    #+#             */
-/*   Updated: 2024/09/24 22:16:07 by migugar2         ###   ########.fr       */
+/*   Updated: 2024/09/25 18:07:08 by migugar2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-// Si queremos hacer referencia a un elemento al que esta apuntando nuestro
-// puntero a un elemento de una t_list podemos escribirlo de dos formas:
-// elem->content o (*elem).content
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
